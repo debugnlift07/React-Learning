162c8b09fab0c3dd9079ab474970ad2312c52933
Naveen Bisht <33859786+debugbyNB@users.noreply.github.com>
2025-10-23 16:20:07 +0530

    complete the caleder app

---
+++ calender-app/calendar-app/src/App.jsx
@@ -0,0 +1,12 @@
+import CalenderApp from "./Components/CalenderApp"
+import './Components/CalenderApp.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalenderApp/>
+    </div>    
+  )
+}
+
+export default App
